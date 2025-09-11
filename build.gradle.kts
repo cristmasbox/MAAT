4cbc39dc5ebc78519cda39ec83408bc697274cf9
cristmasbox <sonne.zucker@gmx.de>
2025-09-11 09:26:53 +0200

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.android.library") version "8.13.0"
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.blueapps.maat"
+    compileSdk = 36
+
+    defaultConfig {
+
+        minSdk = 16
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    testImplementation("junit:junit4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+}
