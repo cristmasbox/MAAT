aac141ce064f29c9569d362ba6c5292cf750f294
cristmasbox <sonne.zucker@gmx.de>
2025-09-11 10:03:17 +0200

    Initial Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import kotlin.math.sign
+
 plugins {
     id("com.android.library") version "8.13.0"
     id("maven-publish")
+    id("signing")
+    //id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 android {
@@ -26,6 +30,53 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.blueapps"
+                artifactId = "maat"
+                version = "1.0.0"
+
+                pom {
+                    name.set("Mobile API for Ancient Texts (MAAT)")
+                    description.set("Library for rendering egyptian hieroglyphic texts.")
+                    url.set("https://github.com/cristmasbox/MAAT")
+                    licenses {
+                        license {
+                            name.set("GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007")
+                            url.set("https://www.gnu.org/licenses/")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("cristmasbox")
+                            name.set("Georg Schierholt")
+                            email.set("sonne.zucker@gmx.de")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/cristmasbox/MAAT")
+                    }
+                }
+            }
+        }
+    }
+
+    signing {
+        sign(publishing.publications["release"])
     }
 }
 
